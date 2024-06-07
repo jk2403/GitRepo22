b8350482ec83ffbee88f17c7cb019c8d4624b923
Jayakumar Rayala <jayakumar.rayala@infor.com>
2024-06-07 12:52:01 +0530

    first commit

--- src/main/java/one/SeleniumWebDriverSampleProgram.java
+++ src/main/java/one/SeleniumWebDriverSampleProgram.java
@@ -3,11 +3,13 @@ package one;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SeleniumWebDriverSampleProgram {
 
 	public static void main(String[] args) {
 		
-		
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.google.com");
